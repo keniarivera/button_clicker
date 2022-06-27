8c8bdd9c424d9e11e6d27d8f7513ad5e9fd7d71f
Kenia Rivera-Vazquez <keniarivera1717@gmail.com>
2022-06-27 16:04:38 -0700

    added button clicker assignment

---
+++ buttonClicker.js
@@ -0,0 +1,14 @@
+function uploadDef(element){
+    console.log("Wazzaaaa", element);
+    element.innerText = "Logout";
+}
+// uploadDef()
+
+function removeElement(element){
+    // console.log("trying to remove...wazzaa");
+    element.remove();
+}
+// removeElement()
+
+// alert is a built in function
+// alert()
